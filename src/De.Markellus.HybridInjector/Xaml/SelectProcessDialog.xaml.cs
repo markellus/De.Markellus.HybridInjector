e7534a7dd2c349f2121ae19b9e7912c5e7d651ef
Markellus <gmx@marcel-bulla.de>
2018-06-04 22:17:47 +0100

    Fixed lags caused by outdated process selection code

--- src/De.Markellus.HybridInjector/Xaml/SelectProcessDialog.xaml.cs
+++ src/De.Markellus.HybridInjector/Xaml/SelectProcessDialog.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.Design.Serialization;
 using System.Diagnostics;
 using System.Drawing;
+using System.Management;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
 using System.Windows.Media.Imaging;
@@ -23,7 +27,7 @@ namespace De.Markellus.HybridInjector.Xaml
 
         public Process GetSelected()
         {
-            return ((ProcessItem)Selection.SelectedItem).Process;
+            return ((ProcessItem) Selection.SelectedItem).Process;
         }
     }
 
@@ -31,123 +35,123 @@ namespace De.Markellus.HybridInjector.Xaml
     {
         public ObservableCollection<ProcessItem> Root
         {
-            get { return GetValue<ObservableCollection<ProcessItem>>("Root"); }
-            set { SetValue("Root", value); }
+            get => GetValue<ObservableCollection<ProcessItem>>("Root");
+            set => SetValue("Root", value);
         }
-
-        DispatcherTimer _timProcessWatcher;
 
         public SelectProcessDialogViewModel()
         {
             Root = new ObservableCollection<ProcessItem>();
 
-            _timProcessWatcher = new DispatcherTimer();
-            _timProcessWatcher.Interval = TimeSpan.FromMilliseconds(2000);
-            _timProcessWatcher.Tick += GetProcesses;
-            _timProcessWatcher.Start();
-            GetProcesses(null, new EventArgs());
-        }
-
-        private void GetProcesses(object sender, EventArgs e)
-        {
             List<Process> processes = new List<Process>(Process.GetProcesses());
 
             foreach (Process p in processes)
             {
-                if (p.MainWindowTitle == "")
-                {
-                    continue;
-                }
-
-                bool contains = false;
-                foreach (ProcessItem i in Root)
-                {
-                    if (i.Process.Id == p.Id)
-                    {
-                        contains = true;
-                    }
-                }
-
-                if (!contains)
-                {
-                    try
-                    {
-                        Root.Add(new ProcessItem(p));
-                    }
-                    catch { }
-                }
-            }
-
-            List<ProcessItem> old = new List<ProcessItem>();
-
-            foreach (ProcessItem p in Root)
-            {
                 try
                 {
-                    if (p.Process.HasExited)
+                    if (p.MainWindowTitle != "" && p.MainModule.FileName != "")
                     {
-                        old.Add(p);
+                        Root.Add(new ProcessItem(p, this));
                     }
                 }
-                catch { }
+                catch
+                {
+                }
             }
 
-            foreach (ProcessItem p in old)
+            ManagementEventWatcher startWatch =
+                new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            startWatch.EventArrived += new EventArrivedEventHandler(OnProcessCreated);
+            startWatch.Start();
+        }
+
+        private void OnProcessCreated(object sender, EventArrivedEventArgs e)
+        {
+            try
             {
-                Root.Remove(p);
+                Process p = Process.GetProcessById(Convert.ToInt32((uint)e.NewEvent.Properties["ProcessID"].Value));
+                //Wait until process is loaded
+                Thread.Sleep(2000);
+                if (p.MainWindowTitle != "" && p.MainModule.FileName != "")
+                {
+                    DispatchService.Invoke(() => { Root.Add(new ProcessItem(p, this)); });
+                }
+            }
+            catch(Exception ex)
+            {
+#if (DEBUG)
+                //MessageBox.Show(ex.Message);
+#endif
             }
         }
     }
 
-
     public class ProcessItem : BaseClass
     {
         DispatcherTimer _timProcessWatcher;
+        private SelectProcessDialogViewModel _baseModel;
 
         public Process Process
         {
-            get { return GetValue<Process>("Process"); }
-            set { SetValue("Process", value); }
+            get => GetValue<Process>("Process");
+            set => SetValue("Process", value);
         }
 
         public string Title
         {
-            get { return GetValue<string>("Title"); }
-            set { SetValue("Title", value); }
+            get => GetValue<string>("Title");
+            set => SetValue("Title", value);
         }
 
         public BitmapSource Icon
         {
-            get { return GetValue<BitmapSource>("Icon"); }
-            set { SetValue("Icon", value); }
+            get => GetValue<BitmapSource>("Icon");
+            set => SetValue("Icon", value);
         }
 
-        public ProcessItem(Process p)
+        public ProcessItem(Process p, SelectProcessDialogViewModel baseModel)
         {
-            Process = p;
+            Process    = p;
+            _baseModel = baseModel;
 
             Icon i = System.Drawing.Icon.ExtractAssociatedIcon(p.MainModule.FileName);
-            this.Icon = Imaging.CreateBitmapSourceFromHIcon(i.Handle, System.Windows.Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(i.Width, i.Height));
+            this.Icon = Imaging.CreateBitmapSourceFromHIcon(i.Handle, System.Windows.Int32Rect.Empty,
+                BitmapSizeOptions.FromWidthAndHeight(i.Width, i.Height));
             this.Title = "Loading...";
 
-            _timProcessWatcher = new DispatcherTimer();
-            _timProcessWatcher.Interval = TimeSpan.FromMilliseconds(1000 / 5);
+            _timProcessWatcher = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(200)};
             _timProcessWatcher.Tick += Refresh;
             _timProcessWatcher.Start();
+        }
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            _baseModel.Root.Remove(this);
+            _timProcessWatcher.Stop();
         }
 
         private void Refresh(object sender, EventArgs e)
         {
             try
             {
+                if (Process.HasExited)
+                {
+                    _baseModel.Root.Remove(this);
+                    _timProcessWatcher.Stop();
+                    return;
+                }
                 string title = Process.MainWindowTitle;
                 if (title.Length > 47)
                 {
                     title = title.Substring(0, 44) + "...";
                 }
+
                 this.Title = title;
             }
-            catch { }
+            catch
+            {
+            }
         }
     }
 }
+
